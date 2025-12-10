ef0b3d3ae5385068f9574c15fb628d634998021f
Carlos Moreno M. <c.morenomontesdeoca@gmail.com>
2025-12-10 23:02:18 +0100

    build(backend-ml): force init at execution

--- backend-ml/main.py
+++ backend-ml/main.py
@@ -6,6 +6,8 @@ Initializes the app and includes all routers.
 from fastapi import FastAPI
 from boe_ingestion.router import router as boe_router
 from rag_agent.router import router as rag_router
+import uvicorn
+import os
 
 app = FastAPI(
     title="ML Backend Service",
@@ -24,7 +26,5 @@ app.include_router(boe_router)
 app.include_router(rag_router)
 
 if __name__ == "__main__":
-    # Importante: host="0.0.0.0" para que Railway lo vea
-    # Importante: leer el puerto de la variable de entorno PORT
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
